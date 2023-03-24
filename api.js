3f3364435cc403c84b696c0a02559321f2fb01db
JettLayer <polluelin.00@gmail.com>
2023-03-23 20:55:26 -0700

    update

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const data = await axios.get(
-    "https://eldenring.fanapis.com/api/bosses"
-);
-datos = data.data;
-console.log(data);
-return datos;
+const callFetch = async () => {
+    const pokemonData = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/`
+    );
+    return callFetch(pokemonData);
+  };
